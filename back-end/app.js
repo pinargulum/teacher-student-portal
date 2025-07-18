edd02d86edc3183116e1d2d327cd06d3458d21b6
unknown <pinargulum@hotmail.com>
2025-07-18 02:43:50 -0400

    update

--- back-end/app.js
+++ back-end/app.js
@@ -1,18 +1,17 @@
+require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const dotenv = require("dotenv");
+
 const mainRouter = require("./routes/index");
 const { verifyToken, checkRole } = require("./middleware/auth");
 
-dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3001;
+
 mongoose
   .connect("mongodb://127.0.0.1:27017/demo_db")
-  .then(() => {
-    console.log("db is connected");
-  })
+  .then(() => console.log("db is connected"))
   .catch(console.error);
 
 app.use(cors());
@@ -20,7 +19,6 @@ app.use(express.json());
 
 app.use("/", mainRouter);
 
-
 app.listen(PORT, () => {
-  console.log("server is running");
+  console.log(`server is running on port ${PORT}`);
 });
